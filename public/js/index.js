e4c70e96105f6fcd8e7df03b9295a1d89570fad0
claramarsango <claraxmarsango@gmail.com>
2023-01-31 23:04:02 +0100

    create src file and include character class modules

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,5 @@
-export {};
+import { Character } from "./src/character.js";
+import { King } from "./src/king.js";
+import { Fighter } from "./src/fighter.js";
+import { Advisor } from "./src/advisor.js";
+import { Shielder } from "./shielder.js";
