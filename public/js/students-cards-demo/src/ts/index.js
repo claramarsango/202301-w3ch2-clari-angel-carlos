3f70fa0f1878eb143f98b1a15492175d7bb1cc5b
claramarsango <claraxmarsango@gmail.com>
2023-01-31 22:59:45 +0100

    delete unwanted files from root

--- public/js/students-cards-demo/src/ts/index.js
+++
@@ -1,8 +0,0 @@
-import MainHeader from "./components/Header/Header.js";
-import StudentCard from "./components/StudentCard/StudentCard.js";
-const appContainer = document.querySelector(".app");
-const header = new MainHeader(appContainer, "Welcome to my components demo");
-header.render();
-const zhenzhi = new StudentCard(appContainer, "Zhenzhi", 0);
-zhenzhi.render();
-console.log(header);
